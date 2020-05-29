dae2875fb937f3d40228615ba2d9eb045838cfe7
Jose Araujo <suportj999@gmail.com>
2020-05-29 11:59:42 -0700

    [update] login api

--- lib/blocs/login/login_bloc.dart
+++ lib/blocs/login/login_bloc.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 
+import 'package:Viiddo/apis/api_service.dart';
 import 'package:bloc/bloc.dart';
 
 import 'login_event.dart';
 import 'login_state.dart';
 
 class LoginScreenBloc extends Bloc<LoginScreenEvent, LoginScreenState> {
-  int resourceID;
+  ApiService _apiService = ApiService();
   @override
   LoginScreenState get initialState => LoginScreenState();
 
@@ -20,7 +21,13 @@ class LoginScreenBloc extends Bloc<LoginScreenEvent, LoginScreenState> {
   Stream<LoginScreenState> _login(Login event) async* {
     yield state.copyWith(isLoading: true);
     try {
-      yield LoginSuccess();
+      bool isLogin =
+          await _apiService.accountLogin(event.username, event.password);
+      if (isLogin) {
+        yield LoginSuccess();
+      } else {
+        yield LoginScreenFailure(error: 'error');
+      }
     } catch (error) {
       yield LoginScreenFailure(error: error);
     } finally {
